19b0388915ef278c8df154627f1202cc3cb88682
Nathan <kongnathan@gmail.com>
2016-11-19 21:21:51 -0500

    Read training images and labels

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ def main():
 	labelsFile = 'Data/train.csv'
 	data = DataTable(pathToImages, labelsFile)
 	data.processImages()
+	data.readLabels()
+	i=0
+	for line in data.labels:
+		i+=1
+		if i==11:
+			break
+		print line
 
 if __name__ == '__main__':
 	main()
--- util.py
+++ util.py
@@ -1,4 +1,5 @@
-import os
+import os, csv, sys
+import numpy as np
 from skimage.io import imread
 
 class DataTable:
@@ -7,13 +8,30 @@ class DataTable:
 		# targetPath is csv file containing labels
 		self.images = imgPath + '/'
 		self.targets = targetPath
+		self.data = None
+		self.labels = None
 
-	def readTarget(self):
-		pass
+	def readLabels(self):
+		# Reads labels file for the training data
+		# [ [number, label], [number, label], ... , [number, label] ]
+		try:
+			self.labels = np.genfromtxt(self.targets, delimiter=',')
+			# Delete the third column and first row (useless)
+			self.labels = np.delete(self.labels, 2, axis=1)
+			self.labels = np.delete(self.labels, 0, axis=0)
+		except:
+			print("Incorrect format or file.")
+			sys.exit(1)
 
 	def processImages(self):
+		# Reads training images
+		# Data shape: [num samples, height, width, channels]
 		print("Processing images...")
+		tmp = list()
 		for filename in os.listdir(self.images):
 			if filename.endswith(".jpg"):
 				t = imread(self.images + filename)
+				tmp.append(t)
+		self.data = np.array(tmp)
 		print("Done processing images.")
+

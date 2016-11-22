dd682a39be9ba194957b2e42dc903a4fef2f9e48
Nathan <kongnathan@gmail.com>
2016-11-22 18:16:00 -0500

    Added training label distribution and modified code to get the distribution

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from util import DataTable, ShowImage, Plot2D
-from preprocess import DoPCA
+from util import DataTable, ShowImage, Plot2D, LoadData
+from preprocess import DoPCA, GetDataDistribution
 
 
 def printSomeLabels(labels, number):
@@ -27,49 +27,18 @@ def dispSomeImages(images, number):
 		ShowImage(image, i)
 
 
-def loadData(dataDir):
-	dataDir += '/'
-	trainData = None
-	trainLabels = None
-	testData = None
-	for f in os.listdir(dataDir):
-		print f
-		if f.endswith(".npz"):
-			data = np.load(dataDir + f)
-
-			# Training set
-			if f.startswith('train'):
-				if trainData == None:
-					trainData = data['inputs_train']
-				else:
-					trainData = np.vstack((trainData, data['inputs_train']))
-
-				if trainLabels == None:
-					trainLabels = data['targets_train']
-				else:
-					trainLabels = np.vstack((trainLabels, data['targets_train']))
-
-			# Test set
-			elif f.startswith('test'):
-				if testData == None:
-					testData = data['inputs_test']
-				else:
-					testData = np.vstack((testData, data['inputs_test']))
-
-			# Unsupported data type
-			else:
-				sys.exit(1)
-
-	print trainData.shape
-	print trainLabels.shape
-	print testData.shape
-	return trainData, trainLabels, testData
-
-
 def main(debug=0):
 
 	dataDir = 'Data/NPZ_data/'
-	loadData(dataDir)
+	listOfTrainingSetFiles = ['train_1_1000.npz', 'train_1001_2000.npz', 'train_2001_3000.npz', \
+							  'train_3001_4000.npz', 'train_4001_5000.npz', 'train_5001_6000.npz', \
+							  'train_6001_7000.npz']
+
+	allLabels, numUniqueLabels = GetDataDistribution(dataDir, listOfTrainingSetFiles)
+
+	plt.figure()
+	plt.hist(allLabels, numUniqueLabels)
+	plt.show()
 
 #	# Preprocess
 #	lowDimInputs = DoPCA(flatInputs, numComponents=2)
--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,8 @@
 import numpy as np
 from sklearn.decomposition import PCA
+from collections import defaultdict
+from util import LoadData
+
 
 def DoPCA(inputs, numComponents=10):
 	# Performs PCA and transforms data into numComponents dimension space
@@ -10,5 +13,26 @@ def DoPCA(inputs, numComponents=10):
 	print(pca.explained_variance_ratio_)
 
 	return lowDimInputs
+
+
+def GetDataDistribution(dataDir, listOfFiles):
+	# Returns numpy array of labels for all training data
+
+	allLabels = np.empty(shape=(0,0))
+	for datafile in listOfFiles:
+		print 'Reading ', datafile, '...'
+		datafiledir = dataDir + '/' + datafile
+		data = LoadData(datafiledir, 'train')
+		labels = data['targets_train']
+		assert labels.shape[1] == 2
+		labels = labels[:,1]
+		if allLabels.shape == (0,0):
+			allLabels = labels
+		else:
+			allLabels = np.hstack((allLabels,labels))
+
+	np.savetxt('labels.out', allLabels.T, delimiter=',')
+	numUnique = len(np.unique(allLabels))
+	return allLabels, numUnique
 
 
